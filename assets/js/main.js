5e73a08560da8fcb109bffb78ef93e0f7e0dd139
Sebastián Cubillos <97460797+GoukiET@users.noreply.github.com>
2022-08-18 00:29:58 -0400

    Add files via upload
    Primer commit

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+let productos = [];
+
+let producto = {
+    
+}
+
+function agregarProducto(){ //CREATE
+
+}
+
+function obtenerProductos(){ //READ
+
+}
+
+function editarProducto(){ //UPDATE
+
+}
+
+function eliminarProducto(){ //DELETE
+
+}
+
+function filtrarProducto(){
+
+}
+
+function ordenarProductos(){
+
+}
+
